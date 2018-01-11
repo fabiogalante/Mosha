c51e900ee2c96e49d6a3c2aa60d9c3f6c9b4e8df
Fabio Galante Mans <fabio.mans@superdigital.com.br>
2018-01-11 18:00:10 -0200

    Curso Mosha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import {AppComponent} from './app.component';
 import {CoursesComponent} from './courses.component';
 import { CursosComponent } from './cursos/cursos.component';
 import {CursosService} from './cursos/cursos.service';
+import { AuthorComponent } from './author/author.component';
+import {AuthorService} from './author.service';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CursosComponent
+    CursosComponent,
+    AuthorComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [
-    CursosService
+    CursosService,
+    AuthorService
   ],
   bootstrap: [AppComponent]
 })
