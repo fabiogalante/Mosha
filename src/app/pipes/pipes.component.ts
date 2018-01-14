19ca7c15b9efe34bbe75f4dfb24ce6fa6ed5e8bc
Fabio Galante Mans <fabiogalantemans@gmail.com>
2018-01-14 21:37:37 -0200

    Pipes

---
+++ src/app/pipes/pipes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  template: `
+    {{ course.title | uppercase | lowercase }} <br/>
+    {{ course.students | number }} <br/>
+    {{ course.rating | number:'2.1-1' }} <br/>
+    {{ course.price | currency:'BRL':true:'3.2-2' }} <br/>
+    {{ course.releaseDate | date:'shortDate' }} 
+    `
+
+})
+export class PipesComponent implements OnInit {
+
+
+  course = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
