0c0ad1f2250443cda33092350fc9347b5c9a5d05
Fabio Galante Mans <fabio.mans@superdigital.com.br>
2018-02-07 11:12:02 -0200

    Validating the Form Upon Submit

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {UsernameValidators} from './username.validators';
 
 
 @Component({
@@ -11,39 +10,18 @@ import {UsernameValidators} from './username.validators';
 
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace],
-      UsernameValidators.shouldBeUnique
-    ),
-    password: new FormControl('', Validators.required)
+   account: new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('', Validators.required)
+    })
   });
 
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 
-  get password() {
-    return this.form.get('password');
-  }
 
-  login() {
-    this.form.setErrors({
-      invalidLogin: true
-    });
-  }
-
-  onSave($event) {
-
-    let teste = 10;
-    let teste2 = teste;
-
-    console.log(teste2);
-
-    console.log($event);
-  }
 }
 
 
