bd910274d91ed00c3726fc5d586c3b3617953e6b
Fabio Galante Mans <fabio.mans@superdigital.com.br>
2018-02-07 09:50:07 -0200

    Asynchhronous Operations Asynchronous Validation

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -4,4 +4,15 @@ export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
     return (control.value as string).indexOf(' ') >= 0 ? {cannotContainSpace: true} : null;
   }
+
+  static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'mosh')
+          resolve({ shouldBeUnique: true });
+        else
+          resolve(null);
+      }, 2000);
+    });
+  }
 }
